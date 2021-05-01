a1189590374897a65ba910153ce0bc2791f24e3d
AleksandarBacko <backo.backovic94@gmail.com>
2021-05-01 14:23:26 -0700

    git works 2

--- java_programming/src/day41_arraylist/practiceGit.java
+++ java_programming/src/day41_arraylist/practiceGit.java
@@ -3,5 +3,6 @@ package day41_arraylist;
 public class practiceGit {
     public static void main(String[] args) {
         System.out.println("git works");
+        System.out.println("sdaasdasdasd");
     }
 }
