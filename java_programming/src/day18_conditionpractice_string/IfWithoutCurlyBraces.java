859279fa4b17694815f7da74abd3c0f7a630ff7a
AleksandarBacko <backo.backovic94@gmail.com>
2021-05-27 16:57:34 -0700

    new change brunch

---
+++ java_programming/src/day18_conditionpractice_string/IfWithoutCurlyBraces.java
@@ -0,0 +1,12 @@
+package day18_conditionpractice_string;
+
+public class IfWithoutCurlyBraces {
+    public static void main(String[] args) {
+        String todayClass="java";
+        if (todayClass.equals("java"))
+            System.out.println("java is fun");
+        else
+            System.out.println("no java");
+
+    }
+}
---
+++ java_programming/src/day47_constructor/makeAddres.java
@@ -0,0 +1,14 @@
+package day47_constructor;
+
+public class makeAddres {
+    public static void main(String[] args) {
+        Addres moja=new Addres();
+        moja.setStreet("4551 Auhay dr");
+        moja.setCity("Santa Barbara");
+        moja.setState("CA");
+        moja.setZipCode("93110");
+        moja.setCountry("USA");
+        System.out.println(moja.toString());
+
+    }
+}
