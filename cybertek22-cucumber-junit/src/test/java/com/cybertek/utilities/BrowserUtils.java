9942ed649b91d7c72789b612101c75838354208a
AleksandarBacko <backo.backovic94@gmail.com>
2021-07-03 08:47:40 -0700

    gyrhans project

---
+++ cybertek22-cucumber-junit/src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -0,0 +1,13 @@
+package com.cybertek.utilities;
+
+public class BrowserUtils {
+    public static void sleep(int secund) {
+        secund*=1000;
+        try {
+            Thread.sleep(secund);
+        } catch (InterruptedException e) {          //nasloni na sleep i izaberi more options i try/catch -isto radi kao i import nasloni opcija
+            e.printStackTrace();
+        }
+
+    }
+}
